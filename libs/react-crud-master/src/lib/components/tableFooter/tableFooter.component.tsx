75a0a085cc83cc4424abbd5c9377594598d3125d
Milan Bogdanovic <milanbogdanovic11@hotmail.com>
2019-10-28 13:15:43 +0100

    refactor tableFooter to hooks

--- libs/react-crud-master/src/lib/components/tableFooter/tableFooter.component.tsx
+++ libs/react-crud-master/src/lib/components/tableFooter/tableFooter.component.tsx
@@ -1,44 +1,14 @@
-import React, { Component, FormEvent, ChangeEvent } from "react";
-import * as Redux from 'redux'
+import React, { } from "react";
 
 import '../reactCrudMaster/reactCrudMaster.css'
 
-import update from 'immutability-helper'
-import { TableFooterOwnProps, TableFooterStateProps, TableFooterDispatchProps, TableFooterProps, TableFooterState } from "./tableFooter.types";
-import { AppState } from "../../rootReducer";
-import { connect } from "react-redux";
-import * as CurdModalActions from '../crudModal/crudModal.actions'
 import SmTableFooter from './smTableFooter'
 import LgTableFooter from './lgTableFooter';
 
-class TableFooterComponent extends Component<TableFooterProps, TableFooterState>{
-    constructor(props: TableFooterProps) {
-        super(props);
-        this.state = {};
-    }
+export default function TableFooterComponent(props: { tableWidth: number }) {
 
-    render = () => {
+    if (props.tableWidth < 620)
+        return <SmTableFooter tableWidth={props.tableWidth} />;
 
-        if (this.props.tableWidth < 620)
-            return <SmTableFooter tableWidth={this.props.tableWidth} />;
-
-        return <LgTableFooter tableWidth={this.props.tableWidth} />
-    };
+    return <LgTableFooter tableWidth={props.tableWidth} />
 }
-
-const mapDispatchToProps = (dispatch: Redux.Dispatch<Redux.AnyAction>, ownProps: TableFooterOwnProps): TableFooterDispatchProps => {
-    return {
-        openCrudModalToCreate: () => null,
-        openCrudModalToEdit: () => null,
-        openWarningModal: () => null,
-        openYesnoModal: () => null,
-        openVModal: () => null
-    };
-}
-
-const mapStateToProps = (state: AppState, props: TableFooterOwnProps): TableFooterStateProps => {
-    return {
-    } as TableFooterStateProps;
-}
-
-export default connect<TableFooterStateProps, TableFooterDispatchProps, TableFooterOwnProps, AppState>(mapStateToProps, mapDispatchToProps)(TableFooterComponent);
